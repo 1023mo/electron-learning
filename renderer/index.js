5ac7cf97956124382100fe2394bb147f93113e41
1023mo <d1024WEI@163.com>
2019-06-23 23:30:40 +0800

    add

--- renderer/index.js
+++ renderer/index.js
@@ -10,7 +10,7 @@ const renderListHTML = (tracks) => {
   const tracksListHTML = tracks.reduce((html, track) => {
     html += `<li class="music-track list-group-item d-flex justify-content-between align-item">
       <div class="col-10">
-        <i class="fas fa-music mr-2"></i>
+        <i class="fas fa-music mr-2 text-secondary"></i>
         <b>${track.fileName}</b>
       </div>
       <div class="col-2">
@@ -21,7 +21,7 @@ const renderListHTML = (tracks) => {
     return html
   }, '')
   const emptyTrackHTML = '<div class="alert alert-primary"></div>'
-  tracksList.innerHTML = tracks.length ? `<ul class="list-group>${tracksListHTML}</ul>` : emptyTrackHTML
+  tracksList.innerHTML = tracks.length ? `<ul class="list-group">${tracksListHTML}</ul>` : emptyTrackHTML
 }
 ipcRenderer.on('getTracks', (event,tracks) => {
   renderListHTML(tracks)
